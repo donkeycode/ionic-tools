3ae688a4a4156c1a63edb2eaa7e04d77115a68e6
Cedric LOMBARDOT <cedric.lombardot@gmail.com>
2015-05-28 19:27:30 +0200

    Fix bad copy paste

--- build/js/dc.tools.js
+++ build/js/dc.tools.js
@@ -542,13 +542,13 @@ angular
         restrict: 'A',
         priority: 555,
         link: function useCachedImage(scope, element) {
-            ImgCache.isBackgroundCached(element.attr('src'), function onSuccess(path, success) {
+            ImgCache.isBackgroundCached(element, function onSuccess(path, success) {
                 if (success) {
                     // already cached
                     ImgCache.useCachedBackground(element);
                 } else {
                     // not there, need to cache the image
-                    ImgCache.cacheBackground(element.attr('src'), function cacheForNext() {
+                    ImgCache.cacheBackground(element, function cacheForNext() {
                         ImgCache.useCachedBackground(element);
                     });
                 }
--- src/tools/directives/use-cached-bg-image.directive.js
+++ src/tools/directives/use-cached-bg-image.directive.js
@@ -3,13 +3,13 @@ function () {
         restrict: 'A',
         priority: 555,
         link: function useCachedImage(scope, element) {
-            ImgCache.isBackgroundCached(element.attr('src'), function onSuccess(path, success) {
+            ImgCache.isBackgroundCached(element, function onSuccess(path, success) {
                 if (success) {
                     // already cached
                     ImgCache.useCachedBackground(element);
                 } else {
                     // not there, need to cache the image
-                    ImgCache.cacheBackground(element.attr('src'), function cacheForNext() {
+                    ImgCache.cacheBackground(element, function cacheForNext() {
                         ImgCache.useCachedBackground(element);
                     });
                 }
